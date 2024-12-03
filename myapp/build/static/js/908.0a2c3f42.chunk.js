6f172709cba5b5c1d1506e0d6238ff4f0a97a20a
anaidachan <anaidaambilissery@gmail.com>
2024-12-03 18:53:57 +0530

    updates

--- myapp/build/static/js/908.0a2c3f42.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmyapp=self.webpackChunkmyapp||[]).push([[908],{908:(p,e,s)=>{s.r(e),s.d(e,{default:()=>a});const a={}}}]);
-//# sourceMappingURL=908.0a2c3f42.chunk.js.map
--- myapp/src/component/index.js
+++ myapp/src/component/index.js
@@ -1,16 +1,8 @@
-import axios from 'axios';
-let currentMode="PROD";
-let currentBackendUrl="";
-if(currentMode==="PROD"){
-    currentBackendUrl="https://nodejs-uqkf.onrender.com/api/v1";
-}else if(currentMode==="NONPROD"){
-    currentBackendUrl= "https://nodejs-uqkf.onrender.com/api/v1";
-}else{
-    currentBackendUrl="http://localhost:8000/api/v1";
-}
- const api=axios.create({
-    baseURL:currentBackendUrl,
-    withCredentials:true,
- });
- export default api;
+import axios from "axios";
 
+const api = axios.create({
+  baseURL: "https://nodejs-uqkf.onrender.com/api/v1",
+  withCredentials: true,
+});
+
+export default api;
