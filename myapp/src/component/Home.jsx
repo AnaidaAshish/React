4628c215b4c4871d29b088d45e440870a02afa7c
anaidachan <anaidaambilissery@gmail.com>
2025-02-06 19:46:40 +0530

    update

--- myapp/src/component/Home.jsx
+++ myapp/src/component/Home.jsx
@@ -1,9 +1,21 @@
-import { useContext } from "react";
-import { AuthContext } from "./context/AuthContext";
-import Viewproducts from "./E-commerce/Viewproducts";
-
-function Home() {
-  <Viewproducts/>
-}
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Styles/Home.css";
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to the Course Management App</h1>
+      <p>Please log in or register to get started.</p>
+      <div>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
