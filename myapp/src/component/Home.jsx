fcf489743967df154a94d27b0b2cba5f139b0292
anaidachan <anaidaambilissery@gmail.com>
2025-02-06 19:50:09 +0530

    update

--- myapp/src/component/Home.jsx
+++ myapp/src/component/Home.jsx
@@ -1,21 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Styles/Home.css";
-const Home = () => {
-  return (
-    <div className="home-container">
-      <h1>Welcome to the Course Management App</h1>
-      <p>Please log in or register to get started.</p>
-      <div>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-        <Link to="/register">
-          <button>Register</button>
-        </Link>
-      </div>
-    </div>
-  );
-};
+import Viewproducts from "./E-commerce/Viewproducts";
+
+function Home() {
+  <Viewproducts/>
+}
 
 export default Home;
